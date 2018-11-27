021bbb89f14d897b2948d6ba18d00d759c6fe748
aca810 <alex810_castellanos@hotmail.com>
2018-11-26 18:48:16 -0600

    Fix bug xoai crash if exists items in embargo

--- dspace-oai/src/main/java/org/dspace/xoai/app/XOAI.java
+++ dspace-oai/src/main/java/org/dspace/xoai/app/XOAI.java
@@ -435,8 +435,8 @@ public class XOAI {
 
         List<ResourcePolicy> policies = authorizeService.getPoliciesActionFilter(context, item, Constants.READ);
         for (ResourcePolicy policy : policies) {
-            if (policy.getGroup().getName().equals("Anonymous")) {
-                
+            if (policy.getGroup() != null && policy.getGroup().getName() != null && policy.getGroup().getName().equals("Anonymous")) {
+
                 if (policy.getStartDate() != null && policy.getStartDate().after(new Date())) {
                     
                     return true;
